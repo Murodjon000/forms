037cc29725695c0f1c012ac3915830a0ecc41bb5
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-01 20:06:45 +0500

    added user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
-   validates :username, presence: true
-   validates :email, presence: true
+   before_save { self.email = email.downcase }
+   validates :username, presence: true, length: { maximum: 50 }
+   # rubocop:disable Style/MutableConstant
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+   # rubocop:enable Style/MutableConstant
+   # rubocop:disable Layout/HashAlignment
+   validates :email, presence: true, length: { maximum: 244 },
+                                     format: { with: VALID_EMAIL_REGEX },
+                                     uniqueness: true
+   # rubocop:enable Layout/HashAlignment
+   has_secure_password
+   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
