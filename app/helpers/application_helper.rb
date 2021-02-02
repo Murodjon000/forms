6c883b45e14395a600e6f84d366fcc8dac136e52
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-02 15:17:47 +0500

    changed application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
-    def full_title(title=' ')
-        base_title='Forms And Helpers'
-        if title.empty?
-            base_title
-        else
-            title + ' | ' + base_title
-        end
+  def full_title(title = ' ')
+    base_title = 'Forms And Helpers'
+    if title.empty?
+      base_title
+    else
+      # rubocop:disable Style/StringConcatenation
+      title + ' | ' + base_title
+      # rubocop:enable Style/StringConcatenation
     end
+  end
 end
