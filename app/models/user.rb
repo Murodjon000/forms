a4f237bece06a83b45296965ae7b7d60200807b7
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-02 15:18:04 +0500

    changed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User < ApplicationRecord
-   before_save { self.email = email.downcase }
-   validates :username, presence: true, length: { maximum: 50 }
-   # rubocop:disable Style/MutableConstant
-   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-   # rubocop:enable Style/MutableConstant
-   # rubocop:disable Layout/HashAlignment
-   validates :email, presence: true, length: { maximum: 244 },
-                                     format: { with: VALID_EMAIL_REGEX },
-                                     uniqueness: true
-   # rubocop:enable Layout/HashAlignment
-   has_secure_password
-   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  before_save { self.email = email.downcase }
+  validates :username, presence: true, length: { maximum: 50 }
+  # rubocop:disable Style/MutableConstant
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  # rubocop:enable Style/MutableConstant
+  # rubocop:disable Layout/HashAlignment
+  validates :email, presence: true, length: { maximum: 244 },
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: true
+  # rubocop:enable Layout/HashAlignment
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
