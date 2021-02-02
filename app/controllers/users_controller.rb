579b1e030da9985b88344d0f50ee68ceb50149c0
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-02 15:17:23 +0500

    changed users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,39 @@
 class UsersController < ApplicationController
-    def new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to Forms'
+      redirect_to @user
+    else
+      render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
